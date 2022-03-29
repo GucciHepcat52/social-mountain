9774820929c3b7f41e13a3f197155c47b318deed
Dallin Breen <dallinbreen624@gmail.com>
2022-03-28 18:23:10 -0600

    Added functionality to code to simulate a messaging site

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-import './App.css';
-
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import Post from "./Post/Post";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios
+      .get("https://practiceapi.devmountain.com/api/posts")
+      .then((res) => {
+        this.setState({ posts: res.data });
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then((res) => {
+        this.setState({ posts: res.data });
+      })
+      .catch((error) => console.log(error));
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then((res) => {
+        this.setState({ posts: res.data });
+      })
+      .catch((error) => console.log(error));
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios
+      .post(`https://practiceapi.devmountain.com/api/posts`, { text })
+      .then((res) => {
+        this.setState({ posts: res.data });
+      })
+      .catch((error) => console.log(error));
   }
 
   render() {
@@ -42,9 +64,19 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
-          <Compose />
-          
+          <Compose createPostFn={this.createPost}/>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                text={post.text}
+                date={post.date}
+                id={post.id}
+                updatePostFn={this.updatePost}
+                deletePostFn={this.deletePost}
+              />
+            );
+          })}
         </section>
       </div>
     );
